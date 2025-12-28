ce2e92035a74a16b12ffd61cfc39a64e1308104e
navodyamethmali <methmalidissanayake@gmail.com>
2025-12-28 18:08:25 +0530

    feacture:complete hero,about and skills sections and start project section

---
+++ src/components/ProjectsSection.jsx
@@ -0,0 +1,10 @@
+
+const projects= [{
+    
+}]
+
+export const ProjectsSection = () => {
+    return(
+        <div className=""></div>
+    );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,13 @@
 import { ThemeToggle } from "../components/Themetoggle";
 import { StarBackground } from "../components/StarBackground";
 import { NavBar } from "../components/NavBar";
+import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
+import { SkillsSection } from "../components/SkillsSection";
+import { ProjectsSection } from "../components/ProjectsSection";
+
+
+
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -18,6 +25,9 @@ export const Home = () => {
         {/* main content */}
         <main>
             <HeroSection />
+            <AboutSection />
+            <SkillsSection />
+            <ProjectsSection />
         </main>
 
         {/* {footer} */}
